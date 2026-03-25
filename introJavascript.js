a5dedef3c7cbe28a54e1a6cdfced40955a3e513b
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-25 16:46:09 +0600

    js statment

--- introJavascript.js
+++ introJavascript.js
@@ -2,3 +2,65 @@
 1: What Is Javascript?  
 Ans:  JavaScript is a versatile, high-level programming language that enables interactive content and dynamic functionality on web pages. It is a core technology of the World Wide Web, working alongside HTML (structure) and CSS (style) to create complete web experiences
 */
+
+// Syntax Rules
+
+// How to Declare variables:
+let x = 5;
+let y = 6;
+
+// How to Compute values:
+let z = x + y;
+
+
+// JavaScript Values
+/**
+ The JavaScript syntax defines two types of values:
+
+Literals (Fixed values)
+Variables (Variable values)
+
+JavaScript Literals
+The most important syntax rules for literals (fixed values) are:
+
+Numbers are written with or without decimals:
+ */
+
+// Example
+
+// 10.50
+
+// 1001
+
+/*Strings are text, written within double or single quotes:
+
+Example
+"John Doe"
+
+'John Doe'
+JavaScript Keywords
+JavaScript keywords are used to defines actions to be performed.
+
+The let and const keywords create variables:
+
+Example
+let x = 5;
+
+const fname = "John";
+Note
+JavaScript keywords are case-sensitive.
+
+JavaScript does not interpret LET or Let as the keyword let.
+
+JavaScript Variables
+Variables are containers for storing data values.
+
+Variables must be identified with unique names.
+
+Example
+// Define x as a variable
+let x;
+
+// Assign the value 6 to x
+x = 6;
+*/
---
+++ jsStatements.js
@@ -0,0 +1,50 @@
+/* In JavaScript, a statement is a single instruction for the computer to perform a specific action. A complete JavaScript program is essentially a sequence of these statements, which are executed by the web browser or runtime environment one by one, in the order they are written.*/
+
+
+/*  Example
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+JavaScript Programs
+A computer program is a list of "instructions" to be "executed" by a computer.
+
+These programming instructions are called statements.
+
+Most JavaScript programs contain many statements.
+
+The statements are executed, one by one, in the same order as they are written.
+
+Note
+In HTML, JavaScript programs are executed by the web browser.
+
+JavaScript Statements
+JavaScript statements are composed of:
+
+Values, Operators, Expressions, Keywords, and Comments.
+
+This statement tells the browser to write "Hello Dolly." inside an HTML element with id="demo":
+
+Example
+document.getElementById("demo").innerHTML = "Hello Dolly.";
+Note
+JavaScript programs (and JavaScript statements) are often called JavaScript code.
+
+Semicolons ;
+Semicolons separate JavaScript statements.
+
+Add a semicolon at the end of each executable statement:
+
+Examples
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
+When separated by semicolons, multiple statements on one line are allowed:
+
+a = 5; b = 6; c = a + b;
+On the web, you might see examples without semicolons.
+Ending statements with semicolon is not required, but highly recommended.*/
+
+
+//collect  from w3school
