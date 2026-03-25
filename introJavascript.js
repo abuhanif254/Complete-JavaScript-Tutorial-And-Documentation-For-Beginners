f4909dee1567c26c5ce29b3702864849d42d52b0
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-25 16:29:32 +0600

    intro of javascript

---
+++ introJavascript.js
@@ -0,0 +1,4 @@
+/* 
+1: What Is Javascript?  
+Ans:  JavaScript is a versatile, high-level programming language that enables interactive content and dynamic functionality on web pages. It is a core technology of the World Wide Web, working alongside HTML (structure) and CSS (style) to create complete web experiences
+*/
